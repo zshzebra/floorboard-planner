3ad7e2c7dd82a2ca318962432041ca6365d70a46
Ryder <ryder@retzlaff.family>
2026-01-17 18:25:30 +1100

    feat: Github action for site deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [
     tailwindcss(),
     svelte(),
